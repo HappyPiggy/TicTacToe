f5a04409cae85865f8e5e35a383fad9fa09468e8
Pigggger <296056428@qq.com>
2016-09-11 23:00:28 +0800

    add gui

---
+++ MyTTT/src/OOVersion/GameState.java
@@ -0,0 +1,5 @@
+package OOVersion;
+
+public enum GameState {  // to save as "GameState.java"
+	   PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+	}
---
+++ MyTTT/src/OOVersion/Seed.java
@@ -0,0 +1,4 @@
+package OOVersion;
+public enum Seed { // to save as "Seed.java"
+	   EMPTY, CROSS, NOUGHT
+	}
